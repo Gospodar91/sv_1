cc482cda1b6ca2c2797b4328b4eda7214468befa
unknown <xsergeix@ukr.net>
2023-08-03 09:22:03 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MainPage from "./page/MainPage/MainPage";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <MainPage />
+    </>
+  );
+};
+export default App;
---
+++ src/redux/store.ts
@@ -0,0 +1,13 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+import notesSlice from "./notesSlice";
+import formSlice from "./formSlice";
+
+export const store = configureStore({
+  reducer: {
+    notes: notesSlice,
+    form: formSlice,
+  },
+});
+export type RootState = ReturnType<typeof store.getState>;
+export default store;
